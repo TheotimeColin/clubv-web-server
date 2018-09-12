db1cd6b746f13051e8289130b7c45c64acfaa7c5
TheotimeColin <contact@theotimecolin.com>
2018-09-12 23:05:29 +0200

    MySQL

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const MongoClient = require('mongodb').MongoClient;
+const mysql = require('mysql')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-const url = 'mongodb+srv://Nerloggz:nerlozyss622@clubv-dev-hwg8b.mongodb.net/test?retryWrites=true';
+/*const url = 'mongodb+srv://Nerloggz:nerlozyss622@clubv-dev-hwg8b.mongodb.net/test?retryWrites=true';
 const dbName = 'test';
 
 MongoClient.connect(url, function(err, client) {
@@ -20,10 +21,27 @@ MongoClient.connect(url, function(err, client) {
   const db = client.db(dbName);
  
   client.close();
-});
+});*/
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'fivem_1'
+})
+
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})
 
 app.get('/posts', (req, res) => {
-  res.send([])
+  let search = req.query.search ? req.query.search : ''
+  
+  connection.query(`SELECT * FROM vrp_user_identities WHERE firstname LIKE '%${search}%' OR name LIKE '%${search}%' OR user_id LIKE '%${search}%' ORDER BY user_id DESC LIMIT 300`, function(err, results) {
+    if (err) throw err
+    res.send(results)
+  })
 })
 
 app.listen(process.env.PORT || 8081)
